316c72ec721ae87a57deee773ff717ea323e1eda
Guilherme Augusto Xavier de Faria <gaxfaria@gmail.com>
2020-06-17 18:14:34 -0300

    Desafio dois finalizado

---
+++ desafioDois.js
@@ -0,0 +1,13 @@
+const programador = {
+  nome: 'Guilherme',
+  idade: 28,
+  tecnologias: ['JavaScript', 'C#', 'SQL'],
+};
+
+const tecnologias = {
+  nome: ['Javascript', 'C#', 'SQL'],
+  especialidade: ['Web', 'Desktop', 'Relattórios'],
+};
+
+console.log(`O programador ${programador.nome} tem ${programador.idade} anos e usa
+a ${tecnologias.nome[0]} com especialidade em ${tecnologias.especialidade[0]}`);
